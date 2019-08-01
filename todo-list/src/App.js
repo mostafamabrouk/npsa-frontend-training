fe60a70d847d85bf38eda862560a0bb49264f599
Mostafa Mabrouk <mostafa.mabrouk@code125.com>
2019-08-01 11:21:26 +0200

    Addin Input

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import Input from "./components/Input";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>ToDo List</h1>
+        <div>
+          <Input />
+        </div>
       </header>
     </div>
   );
