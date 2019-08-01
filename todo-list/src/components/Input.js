72fa0a9ba00d2c0177aad56d32d7b0881678f6c1
Mostafa Mabrouk <mostafa.mabrouk@code125.com>
2019-08-01 11:28:30 +0200

    Adding Button

--- todo-list/src/components/Input.js
+++ todo-list/src/components/Input.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = props => {
-  return <input type="text" />;
+  const [value, setValue] = useState("test");
+  console.log("input", value);
+  return (
+    <div>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          console.log("we should add new item to our list", value);
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default Input;
